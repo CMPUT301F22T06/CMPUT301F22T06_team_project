9b8e5c090d871148591590b9431623f3c5bd67fb
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-10-17 17:20:15 -0600

    Removed demo shape project

--- android_studio_project/app/src/main/java/com/example/cmput301f22t06_team_project/Shape.java
+++
@@ -1,8 +0,0 @@
-package com.example.cmput301f22t06_team_project;
-
-public abstract class Shape {
-    int x;
-    int y;
-    String color = "Color";
-    String Red = "Red";
-}
--- android_studio_project/app/src/main/java/com/example/cmput301f22t06_team_project/Star.java
+++
@@ -1,4 +0,0 @@
-package com.example.cmput301f22t06_team_project;
-
-public class Star extends Shape{
-}
