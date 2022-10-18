26e2fcb881104fa912fc57a84147e049c07b2e92
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-10-17 19:00:42 -0600

    Created Ingredient and Recipe classes

---
+++ android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/Ingredient.java
@@ -0,0 +1,8 @@
+package com.git_er_done.cmput301f22t06_team_project;
+
+public abstract class Ingredient {
+
+    public Ingredient() {
+
+    }
+}
---
+++ android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/Recipe.java
@@ -0,0 +1,11 @@
+package com.git_er_done.cmput301f22t06_team_project;
+
+import java.util.ArrayList;
+
+public class Recipe {
+    private ArrayList<Ingredient> ingredients = new ArrayList<>();
+
+    public Recipe(){
+
+    }
+}
