89be00f9ab317042d116239e35224d707d80fdf0
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-10-17 19:34:37 -0600

    Created member variables + getters & setters

--- android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/Recipe.java
+++ android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/Recipe.java
@@ -4,8 +4,53 @@ import java.util.ArrayList;
 
 public class Recipe {
     private ArrayList<Ingredient> ingredients = new ArrayList<>();
+    private String title;
+    private String comments;
+    private String category;
+    private int prep_time;
+    private int servings;
 
     public Recipe(){
 
+    }
+
+    public String get_title() {
+        return title;
+    }
+
+    public void set_title(String title) {
+        this.title = title;
+    }
+
+    public String get_comments() {
+        return comments;
+    }
+
+    public void set_comments(String comments) {
+        this.comments = comments;
+    }
+
+    public String get_category() {
+        return category;
+    }
+
+    public void set_category(String category) {
+        this.category = category;
+    }
+
+    public int get_prep_time() {
+        return prep_time;
+    }
+
+    public void set_prep_time(int prep_time) {
+        this.prep_time = prep_time;
+    }
+
+    public int get_servings() {
+        return servings;
+    }
+
+    public void set_servings(int servings) {
+        this.servings = servings;
     }
 }
