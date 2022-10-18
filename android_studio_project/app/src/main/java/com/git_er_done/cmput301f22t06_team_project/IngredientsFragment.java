300c2e93cfbcbcbcbe210912fd0b48558301eae6
Devin <dheadrick1618@gmail.com>
2022-10-17 20:20:24 -0600

    Added Nav graph, and associated drawer components along with Fragments for each main user page

---
+++ android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/IngredientsFragment.java
@@ -0,0 +1,30 @@
+package com.git_er_done.cmput301f22t06_team_project;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class IngredientsFragment extends Fragment {
+
+    public IngredientsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ingredient, container, false);
+    }
+}
