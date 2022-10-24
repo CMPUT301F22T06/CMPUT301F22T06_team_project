45e6f9aef55f45bc67f2a7916d27075646937433
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-10-24 17:42:53 -0600

    Added new ingredient types

---
+++ android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/models/NonVegIngredient.java
@@ -0,0 +1,8 @@
+package com.git_er_done.cmput301f22t06_team_project.models;
+
+public class NonVegIngredient extends Ingredient {
+    public NonVegIngredient(String name, String desc, String best_before, String location,
+                            String units, String category, Integer amount) {
+        super(name, desc, best_before, location, units, category, amount);
+    }
+}
