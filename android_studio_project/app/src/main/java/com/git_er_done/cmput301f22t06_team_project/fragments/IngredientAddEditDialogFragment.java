b39da45a1801bc9ed2c31c70e8d9b7897c46713a
Devin <dheadrick1618@gmail.com>
2022-10-27 16:51:19 -0600

    Ingredient edit dialog fragment UI set up. Business logic needed. Refactor to include item add needed.

---
+++ android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/fragments/IngredientAddEditDialogFragment.java
@@ -0,0 +1,87 @@
+package com.git_er_done.cmput301f22t06_team_project.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.git_er_done.cmput301f22t06_team_project.R;
+import com.git_er_done.cmput301f22t06_team_project.models.Ingredient;
+
+//https://guides.codepath.com/android/using-dialogfragment  helpful resource
+
+/**
+ * ALL fragment dialog items MUST be from the androidx.fragment.app namespace !
+ */
+public class IngredientAddEditDialogFragment extends DialogFragment {
+    private EditText etName;
+    private EditText etDescription;
+    private DatePicker dpBestBeforeDate;
+    private Spinner spLocation;
+    private EditText etAmount;
+    private Spinner spUnit;
+    private Spinner spCategory;
+
+    public IngredientAddEditDialogFragment(){
+        //Empty constructor required. New instance static constructor below is called upon instantiation
+    }
+
+    //TODO - get bb4 date, month, year and store as seperate string
+    public static IngredientAddEditDialogFragment newInstance(String title, Ingredient selectedIngredient){
+        IngredientAddEditDialogFragment frag = new IngredientAddEditDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        args.putString("name",  selectedIngredient.getName());
+        args.putString("description", selectedIngredient.getDesc());
+//        args.putString("")
+
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_ingredient_add_edit_dialog, container);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Get associated fied from view items
+        etName = (EditText) view.findViewById(R.id.et_ingredient_add_edit_name);
+        etDescription = (EditText) view.findViewById(R.id.et_ingredient_add_edit_description);
+        dpBestBeforeDate = view.findViewById(R.id.dp_ingredient_add_edit_best_before_date);
+        spLocation = view.findViewById(R.id.sp_ingredient_add_edit_location);
+        etAmount = view.findViewById(R.id.et_ingredient_add_edit_amount);
+        spUnit = view.findViewById(R.id.sp_ingredient_add_edit_unit);
+        spCategory = view.findViewById(R.id.sp_ingredient_add_edit_category);
+
+
+        String dialogTitle = getArguments().getString("title", "Default title ");
+
+        //IF WE ARE EDITTING AN EXISTING INGREDIENT - DISPLAY ITS CURRENT ATTRIBUTES
+
+        //Set associate view items to attributes of selected ingredient
+        String name = getArguments().getString("name", "def - no name found");
+        String description = getArguments().getString("description", "def - desc");
+
+        etName.setText(name);
+        etDescription.setText(description);
+
+
+        //IF WE ARE ADDING A NEW INGREDIENT - LEAVE INPUT FIELDS EMPTY TO SHOW HINTS
+
+
+    }
+}
--- android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/fragments/IngredientsFragment.java
+++ android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/fragments/IngredientsFragment.java
@@ -5,23 +5,28 @@ import android.os.Bundle;
 
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Toast;
 
+import com.git_er_done.cmput301f22t06_team_project.IngredientsRecyclerViewInterface;
 import com.git_er_done.cmput301f22t06_team_project.R;
 import com.git_er_done.cmput301f22t06_team_project.controllers.IngredientsRecyclerViewAdapter;
 import com.git_er_done.cmput301f22t06_team_project.models.Ingredient;
 
 import java.util.ArrayList;
 
-public class IngredientsFragment extends Fragment {
+public class IngredientsFragment extends Fragment implements IngredientsRecyclerViewInterface {
 
     ArrayList<Ingredient> testIngredients;
     RecyclerView rvIngredients;
+    IngredientsRecyclerViewAdapter rvAdapter;
 
 
     public IngredientsFragment() {
@@ -48,10 +53,34 @@ public class IngredientsFragment extends Fragment {
 
         testIngredients = Ingredient.createIngredientList();
 
-        IngredientsRecyclerViewAdapter adapter = new IngredientsRecyclerViewAdapter(testIngredients);
-        rvIngredients.setAdapter(adapter);
+        rvAdapter = new IngredientsRecyclerViewAdapter(testIngredients, this);
+        rvIngredients.setAdapter(rvAdapter);
 
         // Inflate the layout for this fragment
         return root;
+    }
+
+    private void showEditDialog(Ingredient selectedIngredient) {
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        IngredientAddEditDialogFragment editNameDialogFragment =
+                IngredientAddEditDialogFragment.newInstance(
+                        "Edit Ingredient Dialog",
+                        selectedIngredient);
+        editNameDialogFragment.show(fm, "fragment_ingredient_add_edit_dialog");
+
+    }
+
+    @Override
+    public void onItemLongClick(int position) {
+        Toast toast = Toast.makeText(getActivity(),
+                "Item at pos: "  + Integer.toString(position) + " selected",
+                Toast.LENGTH_SHORT);
+        toast.show();
+
+        Ingredient selectedIngredient = rvAdapter.getItem(position);
+
+        //Create a dialog displaying all of the selected ingredients attributes
+        showEditDialog(selectedIngredient);
+
     }
 }
