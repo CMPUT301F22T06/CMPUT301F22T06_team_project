cd68640db4a6367055cf26030e61936b7be310ed
Devin <dheadrick1618@gmail.com>
2022-10-28 10:30:14 -0600

    Added practice test class to try implementing github actions CI. Added associated yaml file.

---
+++ android_studio_project/app/src/test/java/com/git_er_done/cmput301f22t06_team_project/IngredientDBHelperTest.java
@@ -0,0 +1,34 @@
+package com.git_er_done.cmput301f22t06_team_project;
+
+import static org.junit.Assert.assertTrue;
+
+import com.git_er_done.cmput301f22t06_team_project.models.Ingredient;
+import com.git_er_done.cmput301f22t06_team_project.models.ingredientTypes.ProteinIngredient;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class IngredientDBHelperTest {
+//    private Ingredient mockIngredient;
+//
+//    public ArrayList<Ingredient> mockIngredientList(){
+//        ArrayList<Ingredient> mockIngredientList = new ArrayList<>();
+//
+//        Ingredient i1 = new ProteinIngredient("Beef", "AAA sirloin", LocalDate.now(),"Pantry", "g", "Protein", 5);
+//
+//        mockIngredientList.add(i1);
+//
+//        return mockIngredientList;
+//    }
+
+
+
+    @Test
+    public void testAddIngredient(){
+
+        assertTrue(false);
+    }
+}
