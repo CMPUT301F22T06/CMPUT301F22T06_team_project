2e8ee47c17cef07119a343d94d8b5b7293a0deda
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-10-31 16:19:23 -0600

    Trying to set up espresso

--- android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/EspressoTests.java
+++ android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/EspressoTests.java
@@ -1,4 +1,10 @@
 package com.git_er_done.cmput301f22t06_team_project;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 public class EspressoTests {
 }
