6137b1e1ad9b6c31e5dc98b5a6846b890a94991b
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-10-31 16:00:23 -0600

    Created Solo unit tests

---
+++ android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/InterfaceTests.java
@@ -0,0 +1,46 @@
+package com.git_er_done.cmput301f22t06_team_project;
+
+import android.app.Activity;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+@RunWith(AndroidJUnit4.class)
+public class InterfaceTests {
+
+    private Solo solo;
+    @Rule
+    public ActivityTestRule<MainActivity> rule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+    }
+
+    @Test
+    public void start() throws Exception {
+        Activity activity = rule.getActivity();
+    }
+
+    @Test
+    public void test() {
+        solo.assertCurrentActivity("", MainActivity.class);
+    }
+}
