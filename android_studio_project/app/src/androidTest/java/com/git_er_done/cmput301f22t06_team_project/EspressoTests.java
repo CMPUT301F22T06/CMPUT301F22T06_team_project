4e5353a00290e914e173ab5512ebcb5cb504bd35
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-10-31 18:02:26 -0600

    Wrote proof of concept test

--- android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/EspressoTests.java
+++ android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/EspressoTests.java
@@ -1,10 +1,25 @@
 package com.git_er_done.cmput301f22t06_team_project;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class EspressoTests {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+    @Test
+    public void firstTest() {
+        onView(withText("RECIPES FRAGMENT")).check(matches(isDisplayed()));
+    }
 }
