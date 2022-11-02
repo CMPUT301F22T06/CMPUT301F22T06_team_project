291392784f1cc797ef27baa448544bf625f6888d
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-11-02 12:46:24 -0600

    Began writing recycler view test

--- android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/EspressoTests.java
+++ android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/EspressoTests.java
@@ -3,9 +3,16 @@ package com.git_er_done.cmput301f22t06_team_project;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import android.view.Gravity;
+
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.DrawerMatchers;
+import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -27,6 +34,13 @@ public class EspressoTests {
 
     @Test
     public void testIngredientRecyclerView() {
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT)))
+                .perform(DrawerActions.open());
+
+        onView(withId(R.id.navigation_view))
+                .perform(NavigationViewActions.navigateTo(R.id.nav_ingredients_menu_item));
+
 
     }
 }
