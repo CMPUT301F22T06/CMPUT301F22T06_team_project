6ba7ee0b1fec385d43c450f08b16af5b4cdbf980
Saheel Sarker <saheel@ualberta.ca>
2022-11-01 23:58:42 -0600

    Made further changes to recipeIngredient

--- android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/models/RecipeIngredient.java
+++ android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/models/RecipeIngredient.java
@@ -1,23 +1,23 @@
 package com.git_er_done.cmput301f22t06_team_project.models;
 
 public class RecipeIngredient {
-    Ingredient ingredient;
+    String name;
     String units;
     int amount;
     String comment;
-    public RecipeIngredient(Ingredient ingredient, String units, int amount, String comment) {
-        this.ingredient = ingredient;
+    public RecipeIngredient(String name, String units, int amount, String comment) {
+        this.name = name;
         this.units = units;
         this.amount = amount;
         this.comment = comment;
     }
 
-    public Ingredient getIngredient() {
-        return ingredient;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setIngredient(Ingredient ingredient) {
-        this.ingredient = ingredient;
+    public String getName() {
+        return name;
     }
 
     public String getUnits() {
@@ -34,10 +34,6 @@ public class RecipeIngredient {
 
     public void setAmount(int amount) {
         this.amount = amount;
-    }
-
-    public String getName(){
-        return ingredient.getName();
     }
 
     public String getComment() {
