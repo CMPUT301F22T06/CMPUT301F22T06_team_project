d154498e1ffd08a5b0a72794391a1740fb6de7a6
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-11-03 18:19:54 -0600

    Removed proof of concept test. Added mel plan recycler view test

--- android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/EspressoTests.java
+++ android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/EspressoTests.java
@@ -3,13 +3,14 @@ package com.git_er_done.cmput301f22t06_team_project;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
+import static androidx.test.espresso.contrib.DrawerMatchers.*;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.view.Gravity;
 
+import androidx.test.espresso.IdlingResource;
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.contrib.DrawerMatchers;
 import androidx.test.espresso.contrib.NavigationViewActions;
@@ -23,24 +24,36 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class EspressoTests {
 
+//    public IdlingResource getIdlingResourceInTest() {
+//        return mIdlingRes;
+//    }
+
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule =
             new ActivityScenarioRule<>(MainActivity.class);
-    // Will change
-    @Test
-    public void firstTest() {
-        onView(withText("RECIPES FRAGMENT")).check(matches(isDisplayed()));
-    }
 
     @Test
     public void testIngredientRecyclerView() {
         onView(withId(R.id.drawer_layout))
                 .check(matches(isClosed(Gravity.LEFT)))
-                .perform(DrawerActions.open());
+                .perform(DrawerActions.open())
+                .check(matches(isOpen(Gravity.LEFT)));
 
         onView(withId(R.id.navigation_view))
                 .perform(NavigationViewActions.navigateTo(R.id.nav_ingredients_menu_item));
 
+        onView(withText("apple")).check(matches(isDisplayed()));
+    }
 
+    @Test
+    public void testMealPlannerRecyclerView() {
+
+        onView(withId(R.id.drawer_layout))
+                .perform(DrawerActions.open());
+
+        onView(withId(R.id.navigation_view))
+                .perform(NavigationViewActions.navigateTo(R.id.nav_meal_planner_menu_item));
+
+        onView(withText("MEAL PLANNER FRAGMENT")).check(matches(isDisplayed()));
     }
 }
