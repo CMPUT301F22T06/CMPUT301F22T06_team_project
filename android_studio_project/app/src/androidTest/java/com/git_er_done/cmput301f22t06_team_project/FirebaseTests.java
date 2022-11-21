fd1e36f9bc942bbe46fc53e1a4dcdbb1befd398c
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-11-21 16:11:31 -0700

    Began implementing Firestore tests

--- android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/FirebaseTests.java
+++ android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/FirebaseTests.java
@@ -1,4 +1,10 @@
 package com.git_er_done.cmput301f22t06_team_project;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+//@Suite.SuiteClasses()
 public class FirebaseTests {
+
 }
