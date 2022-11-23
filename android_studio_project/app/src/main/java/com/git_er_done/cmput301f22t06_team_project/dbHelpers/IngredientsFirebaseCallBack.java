306b60ba5fc3e34b7084b33b2a13948e6704d2db
Devin <dheadrick1618@gmail.com>
2022-11-23 00:52:16 -0700

    Removed unused firebase callbacks

--- android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/dbHelpers/IngredientsFirebaseCallBack.java
+++
@@ -1,7 +0,0 @@
-package com.git_er_done.cmput301f22t06_team_project.dbHelpers;
-
-import com.git_er_done.cmput301f22t06_team_project.models.ingredient.Ingredient;
-
-public interface IngredientsFirebaseCallBack {
-    void onCallback(Ingredient retrievedIngredients);
-}
