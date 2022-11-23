d392aa59bc8d2ec9c384afd993079318188c0be8
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-11-23 15:46:12 -0700

    Update instrumentation tests to use better checks

--- android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/EspressoTests.java
+++ android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/EspressoTests.java
@@ -14,12 +14,19 @@ import static org.hamcrest.Matchers.instanceOf;
 
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.contrib.*;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeMatcher;
 import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
@@ -47,7 +54,12 @@ public class EspressoTests {
         onView(withId(R.id.navigation_view))
                 .perform(NavigationViewActions.navigateTo(R.id.nav_ingredients_menu_item));
 
-        onView(withText("cooking wine")).check(matches(isDisplayed()));
+        ViewInteraction textView = onView(
+                allOf(withText("Ingredients List"),
+                        withParent(allOf(withId(R.id.my_toolbar),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
+                        isDisplayed()));
+        textView.check(matches(withText("Ingredients List")));
     }
 
     @Test
@@ -61,7 +73,12 @@ public class EspressoTests {
         onView(withId(R.id.navigation_view))
                 .perform(NavigationViewActions.navigateTo(R.id.nav_meal_planner_menu_item));
 
-        onView(withText("MEAL PLANNER FRAGMENT")).check(matches(isDisplayed()));
+        ViewInteraction textView = onView(
+                allOf(withText("Meal Planner"),
+                        withParent(allOf(withId(R.id.my_toolbar),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
+                        isDisplayed()));
+        textView.check(matches(withText("Meal Planner")));
     }
 
     @Test
@@ -75,7 +92,12 @@ public class EspressoTests {
         onView(withId(R.id.navigation_view))
                 .perform(NavigationViewActions.navigateTo(R.id.nav_shopping_list_menu_item));
 
-        onView(withText("SHOPPING LIST FRAGMENT")).check(matches(isDisplayed()));
+        ViewInteraction textView = onView(
+                allOf(withText("Shopping List"),
+                        withParent(allOf(withId(R.id.my_toolbar),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
+                        isDisplayed()));
+        textView.check(matches(withText("Shopping List")));
     }
 
     @Test
@@ -88,7 +110,12 @@ public class EspressoTests {
         onView(withId(R.id.navigation_view))
                 .perform(NavigationViewActions.navigateTo(R.id.nav_recipes_menu_item));
 
-        onView(withText("RECIPES FRAGMENT")).check(matches(isDisplayed()));
+        ViewInteraction textView = onView(
+                allOf(withText("Recipes"),
+                        withParent(allOf(withId(R.id.my_toolbar),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
+                        isDisplayed()));
+        textView.check(matches(withText("Recipes")));
     }
 
     @Test
@@ -98,23 +125,56 @@ public class EspressoTests {
                 .perform(DrawerActions.open())
                 .check(matches(isOpen(Gravity.LEFT)));
 
-        onView(withText("I like to eat food")).check(matches(isDisplayed()));
+        ViewInteraction textView = onView(
+                allOf(withText("Menu"),
+                        withParent(withParent(withId(com.google.android.material.R.id.navigation_header_container))),
+                        isDisplayed()));
+        textView.check(matches(withText("Menu")));
 
         onView(withId(R.id.drawer_layout))
                 .perform(DrawerActions.close());
 
-        onView(withText("RECIPES FRAGMENT")).check(matches(isDisplayed()));
+        ViewInteraction textView1 = onView(
+                allOf(withText("Recipes"),
+                        withParent(allOf(withId(R.id.my_toolbar),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
+                        isDisplayed()));
+        textView1.check(matches(withText("Recipes")));
     }
 
     @Test
     public void testIngredientEdit() {
-        onView(withId(R.id.drawer_layout))
-                .perform(DrawerActions.open());
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Drawer Open"),
+                        childAtPosition(
+                                allOf(withId(R.id.my_toolbar),
+                                        childAtPosition(
+                                                withClassName(Matchers.is("android.widget.LinearLayout")),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
 
-        onView(withId(R.id.navigation_view))
-                .perform(NavigationViewActions.navigateTo(R.id.nav_ingredients_menu_item));
+        ViewInteraction navigationMenuItemView = onView(
+                allOf(withId(R.id.nav_ingredients_menu_item),
+                        childAtPosition(
+                                allOf(withId(com.google.android.material.R.id.design_navigation_view),
+                                        childAtPosition(
+                                                withId(R.id.navigation_view),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        navigationMenuItemView.perform(click());
 
-        onView(withId(R.id.fab_ingredient_add)).perform(click());
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.fab_ingredient_add),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_host_fragment),
+                                        0),
+                                1),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
 
         onView(withId(R.id.et_ingredient_add_edit_name)).perform(replaceText("9V"));
         onView(withId(R.id.et_ingredient_add_edit_description)).perform(replaceText("Energizer"));
@@ -142,9 +202,6 @@ public class EspressoTests {
         onView(withId(R.id.navigation_view))
                 .perform(NavigationViewActions.navigateTo(R.id.nav_ingredients_menu_item));
 
-        //onView(withId(R.id.rv_ingredients_list))
-        //        .perform(RecyclerViewActions.actionOnItemAtPosition(0, longClick()));
-
         onView(withId(R.id.rv_ingredients_list))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, longClick()));
 
@@ -170,10 +227,29 @@ public class EspressoTests {
 
         onView(withText("3")).check(matches(isDisplayed()));
 
+        onView(withId(R.id.btn_ingredient_add_edit_cancel))
+                .perform(click());
+
         // Cleanup
 
         onView(withId(R.id.rv_ingredients_list))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, swipeLeft()));
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(com.google.android.material.R.id.snackbar_action), withText("DELETE"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(Matchers.is("com.google.android.material.snackbar.Snackbar$SnackbarLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        materialButton.perform(click());
     }
 
     @Test
@@ -192,37 +268,141 @@ public class EspressoTests {
 
     @Test
     public void testAddIngredient() {
-        onView(withId(R.id.drawer_layout))
-                .perform(DrawerActions.open());
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Drawer Open"),
+                        childAtPosition(
+                                allOf(withId(R.id.my_toolbar),
+                                        childAtPosition(
+                                                withClassName(Matchers.is("android.widget.LinearLayout")),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
 
-        onView(withId(R.id.navigation_view))
-                .perform(NavigationViewActions.navigateTo(R.id.nav_ingredients_menu_item));
+        ViewInteraction navigationMenuItemView = onView(
+                allOf(withId(R.id.nav_ingredients_menu_item),
+                        childAtPosition(
+                                allOf(withId(com.google.android.material.R.id.design_navigation_view),
+                                        childAtPosition(
+                                                withId(R.id.navigation_view),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        navigationMenuItemView.perform(click());
 
-        onView(withId(R.id.fab_ingredient_add))
-                .perform(click());
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.fab_ingredient_add),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_host_fragment),
+                                        0),
+                                1),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
 
-        onView(withId(R.id.et_ingredient_add_edit_name)).perform(replaceText("9V"));
-        onView(withId(R.id.et_ingredient_add_edit_description)).perform(replaceText("Energizer"));
-        onView(withId(R.id.dp_ingredient_add_edit_best_before_date)).perform(PickerActions.setDate(2023, 11, 14));
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.et_ingredient_add_edit_name),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(Matchers.is("android.widget.LinearLayout")),
+                                        0),
+                                1)));
+        appCompatEditText.perform(scrollTo(), replaceText("Battery"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.et_ingredient_add_edit_description),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(Matchers.is("android.widget.LinearLayout")),
+                                        1),
+                                1)));
+        appCompatEditText2.perform(scrollTo(), replaceText("AAA"), closeSoftKeyboard());
 
         onView(withId(R.id.sp_ingredient_add_edit_location)).perform(click());
+        onData(allOf(CoreMatchers.is(instanceOf(String.class)), CoreMatchers.is("pantry"))).inRoot(isPlatformPopup()).perform(click());
 
-        onData(allOf(is(instanceOf(String.class)), is("pantry"))).inRoot(isPlatformPopup()).perform(click()); // select pantry location
-
-        onView(withId(R.id.et_ingredient_add_edit_amount)).perform(replaceText("3"));
         onView(withId(R.id.sp_ingredient_add_edit_unit)).perform(click());
+        onData(allOf(CoreMatchers.is(instanceOf(String.class)), CoreMatchers.is("singles"))).inRoot(isPlatformPopup()).perform(click()); // select oz for units
 
-        onData(allOf(is(instanceOf(String.class)), is("singles"))).inRoot(isPlatformPopup()).perform(click()); // select oz for units
         onView(withId(R.id.sp_ingredient_add_edit_category)).perform(click());
+        onData(allOf(CoreMatchers.is(instanceOf(String.class)), CoreMatchers.is("miscellaneous"))).inRoot(isPlatformPopup()).perform(click());
 
-        onData(allOf(is(instanceOf(String.class)), is("miscellaneous"))).inRoot(isPlatformPopup()).perform(click()); // select vegetable type
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.et_ingredient_add_edit_amount),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(Matchers.is("android.widget.LinearLayout")),
+                                        0),
+                                1)));
+        appCompatEditText3.perform(scrollTo(), replaceText("4"), closeSoftKeyboard());
 
-        onView(withId(R.id.btn_ingredient_add_edit_save)).perform(click());
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_ingredient_add_edit_save), withText("SAVE"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(Matchers.is("android.widget.LinearLayout")),
+                                        6),
+                                1)));
+        appCompatButton.perform(scrollTo(), click());
 
         ViewInteraction textView = onView(
-                allOf(withId(R.id.tv_ingredient_list_item_name), withText("9v"),
-                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                allOf(withId(R.id.tv_ingredient_list_item_name), withText("Battery"),
+                        withParent(withParent(withId(R.id.background))),
                         isDisplayed()));
-        textView.check(matches(withText("9v")));
+        textView.check(matches(withText("Battery")));
+    }
+
+    @Test
+    public void testDeleteIngredient() {
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Drawer Open"),
+                        childAtPosition(
+                                allOf(withId(R.id.my_toolbar),
+                                        childAtPosition(
+                                                withClassName(Matchers.is("android.widget.LinearLayout")),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction navigationMenuItemView = onView(
+                allOf(withId(R.id.nav_ingredients_menu_item),
+                        childAtPosition(
+                                allOf(withId(com.google.android.material.R.id.design_navigation_view),
+                                        childAtPosition(
+                                                withId(R.id.navigation_view),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        navigationMenuItemView.perform(click());
+
+        onView(withId(R.id.rv_ingredients_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, swipeLeft()));
+
+        ViewInteraction textView = onView(
+                allOf(withText("Ingredients List"),
+                        withParent(allOf(withId(R.id.my_toolbar),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
+                        isDisplayed()));
+        textView.check(matches(withText("Ingredients List")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
