bdbdfe3162bfe2d638d22a1b56a1c5b9a9eb95dc
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-11-25 18:41:31 -0700

    Wrote Add and Delete tests

--- android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/DBHelperTests/IngredientDBHelperTest.java
+++ android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/DBHelperTests/IngredientDBHelperTest.java
@@ -1,5 +1,8 @@
 package com.git_er_done.cmput301f22t06_team_project.DBHelperTests;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -7,6 +10,7 @@ import com.git_er_done.cmput301f22t06_team_project.MainActivity;
 import com.git_er_done.cmput301f22t06_team_project.dbHelpers.IngredientDBHelper;
 import com.git_er_done.cmput301f22t06_team_project.models.ingredient.Ingredient;
 
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,42 +25,80 @@ public class IngredientDBHelperTest {
     public ActivityScenarioRule<MainActivity> activityRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
-//    public ArrayList<Ingredient> mockIngredientList(){
-//        ArrayList<Ingredient> mockIngredientList = new ArrayList<>();
-//
-//        Ingredient i1 = new Ingredient("Beef", "AAA sirloin", LocalDate.now(),"Pantry", "g", "Protein", 5);
-//
-//        mockIngredientList.add(i1);
-//
-//        return mockIngredientList;
-//    }
-
     public Ingredient makeIngredient() {
-        return new Ingredient("Steak", "T-Bone", LocalDate.now().plusYears(1),
+        return new Ingredient("steak", "T-Bone", LocalDate.now().plusYears(1),
                 "freezer", "singles", "protein", 2);
     }
 
     @Test
     public void testAddIngredient(){
         IngredientDBHelper.getInstance();
+        ArrayList<String> namesFromDB = new ArrayList<>();
+        ArrayList<Ingredient> ingFromDB;
         Ingredient mockIngredient = makeIngredient();
         IngredientDBHelper.addIngredientToDB(mockIngredient);
 
+        // Allow database time to give data
+        try {
+            Thread.sleep(5000);
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
 
+        ingFromDB = IngredientDBHelper.getIngredientsFromStorage();
+        System.out.println("Data from DB:");
+        for (int i = 0; i < ingFromDB.size(); i++) {
+            System.out.println(ingFromDB.get(i).getName());
+            namesFromDB.add(ingFromDB.get(i).getName());
+        }
+        System.out.println("End of DB data.");
+
+        assertTrue(namesFromDB.contains(mockIngredient.getName().toLowerCase()));
+
+        // Cleanup
+        IngredientDBHelper.deleteIngredientFromDB(mockIngredient, 0);
     }
 
     @Test
     public void testDeleteIngredient(){
+        IngredientDBHelper.getInstance();
+        Ingredient mockIngredient = makeIngredient();
+        ArrayList<String> namesFromDB = new ArrayList<>();
+        ArrayList<Ingredient> ingFromDB;
 
-    }
+        IngredientDBHelper.addIngredientToDB(mockIngredient);
 
-    @Test
-    public void testSearchForIngredient(){
+        // Allow database time to give data
+        try {
+            Thread.sleep(5000);
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
 
-    }
+        ingFromDB = IngredientDBHelper.getIngredientsFromStorage();
+        for (int i = 0; i < ingFromDB.size(); i++) {
+            namesFromDB.add(ingFromDB.get(i).getName());
+        }
+        // Make sure ingredient was actually added
+        assertTrue(namesFromDB.contains(mockIngredient.getName().toLowerCase()));
 
-    @Test
-    public void testGetAllIngredients(){
+        IngredientDBHelper.deleteIngredientFromDB(mockIngredient, 0);
+
+        // Give DB time to actually delete the item
+        try {
+            Thread.sleep(1000);
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ingFromDB = IngredientDBHelper.getIngredientsFromStorage();
+        namesFromDB = new ArrayList<>();
+        System.out.println("After clear: " + namesFromDB);
+        for (int i = 0; i < ingFromDB.size(); i++) {
+            namesFromDB.add(ingFromDB.get(i).getName());
+        }
+
+        assertFalse(namesFromDB.contains(mockIngredient.getName().toLowerCase()));
 
     }
 
