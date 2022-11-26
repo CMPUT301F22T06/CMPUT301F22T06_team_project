2db5c60f990ecefada6e8541cb5dde3fd2b27855
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-11-25 20:10:41 -0700

    Wrote addRecipe test

--- android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/DBHelperTests/RecipeDBHelperTest.java
+++ android_studio_project/app/src/androidTest/java/com/git_er_done/cmput301f22t06_team_project/DBHelperTests/RecipeDBHelperTest.java
@@ -1,16 +1,55 @@
 package com.git_er_done.cmput301f22t06_team_project.DBHelperTests;
 
+import static org.junit.Assert.assertTrue;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.git_er_done.cmput301f22t06_team_project.MainActivity;
 import com.git_er_done.cmput301f22t06_team_project.dbHelpers.RecipeDBHelper;
 import com.git_er_done.cmput301f22t06_team_project.models.recipe.Recipe;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+
+@RunWith(AndroidJUnit4.class)
 public class RecipeDBHelperTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    public Recipe makeRecipe() {
+        return new Recipe("unit test", "Comments", "category", 1, 1);
+    }
 
     @Test
     public void testAddRecipe() {
-        Recipe testRecipe = new Recipe("unit test", "Comments", "category", 1, 1);
-        RecipeDBHelper.addRecipe(testRecipe);
+        RecipeDBHelper.getInstance();
+        ArrayList<String> nameFromDB = new ArrayList<>();
+        ArrayList<Recipe> recFromDB;
+        Recipe mockRecipe = makeRecipe();
+        RecipeDBHelper.addRecipe(mockRecipe);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        recFromDB = RecipeDBHelper.getRecipesFromStorage();
+        for (int i = 0; i < recFromDB.size(); i++) {
+            nameFromDB.add(recFromDB.get(i).getTitle());
+        }
+
+        assertTrue(nameFromDB.contains(mockRecipe.getTitle()));
+
+        // Cleanup
+        RecipeDBHelper.deleteRecipe(mockRecipe, 0);
+
     }
 
     @Test
@@ -19,17 +58,5 @@ public class RecipeDBHelperTest {
 
     @Test
     public void testModifyRecipeInDB() {
-    }
-
-    @Test
-    public void testSetRecipesAdapter() {
-    }
-
-    @Test
-    public void testSetRecipeIngredientAdapter() {
-    }
-
-    @Test
-    public void testEventChangeListener() {
     }
 }
