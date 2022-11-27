d2d3883f8ed7dd4cc0b2bf5dde2f4d3fc9223c04
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-11-26 21:12:52 -0700

    Implemented Meal get/set tests

--- android_studio_project/app/src/test/java/com/git_er_done/cmput301f22t06_team_project/ModelTests/MealTest.java
+++ android_studio_project/app/src/test/java/com/git_er_done/cmput301f22t06_team_project/ModelTests/MealTest.java
@@ -1,5 +1,58 @@
 package com.git_er_done.cmput301f22t06_team_project.ModelTests;
 
+import static org.junit.Assert.assertEquals;
+
+import com.git_er_done.cmput301f22t06_team_project.models.ingredient.Ingredient;
+import com.git_er_done.cmput301f22t06_team_project.models.meal.Meal;
+import com.git_er_done.cmput301f22t06_team_project.models.recipe.Recipe;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class MealTest {
+    private final Random rand = new Random();
+
+    public Recipe makeRecipe() {
+        return new Recipe("unit test", "test", "grain", rand.nextInt(10), rand.nextInt(10));
+    }
+    public Ingredient makeIngredient() {
+        return new Ingredient("unit test", "test", LocalDate.now().plusMonths(1), "pantry", "oz", "grain", rand.nextInt(10));
+    }
+
+    public Meal makeMeal() {
+        return new Meal(new ArrayList<>(), new ArrayList<>(), LocalDate.now().plusDays(1));
+    }
+
+    @Test
+    public void testAddGetIngredients() {
+        Meal mockMeal = makeMeal();
+        Ingredient firstIngredient = makeIngredient();
+        Ingredient secondIngredient = makeIngredient();
+        mockMeal.addIngredientToMeal(firstIngredient);
+        mockMeal.addIngredientToMeal(firstIngredient);
+        mockMeal.addIngredientToMeal(secondIngredient);
+
+        assertEquals(firstIngredient, mockMeal.getIngredientsFromMeal().get(0));
+        assertEquals(firstIngredient, mockMeal.getIngredientsFromMeal().get(1));
+        assertEquals(secondIngredient, mockMeal.getIngredientsFromMeal().get(2));
+    }
+
+    @Test
+    public void testAddGetRecipes() {
+        Meal mockMeal = makeMeal();
+        Recipe firstRecipe = makeRecipe();
+        Recipe secondRecipe = makeRecipe();
+        mockMeal.addRecipeToMeal(firstRecipe);
+        mockMeal.addRecipeToMeal(firstRecipe);
+        mockMeal.addRecipeToMeal(secondRecipe);
+
+        assertEquals(firstRecipe, mockMeal.getRecipesFromMeal().get(0));
+        assertEquals(firstRecipe, mockMeal.getRecipesFromMeal().get(1));
+        assertEquals(secondRecipe, mockMeal.getRecipesFromMeal().get(2));
+    }
 
 }
