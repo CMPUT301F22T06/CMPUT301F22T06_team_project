3d88fab50e7742cb1950da476f3fb1cd60f028da
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-11-27 18:15:16 -0700

    Init commit

---
+++ android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/customViews/RecipeMealItemView.java
@@ -0,0 +1,11 @@
+package com.git_er_done.cmput301f22t06_team_project.customViews;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class RecipeMealItemView extends LinearLayout {
+
+    public RecipeMealItemView(Context context) {
+        super(context);
+    }
+}
