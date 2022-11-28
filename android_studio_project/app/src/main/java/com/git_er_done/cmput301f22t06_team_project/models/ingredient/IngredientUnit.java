0d7e987578740cbc29f33bca1f86303dba8c8a5e
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-11-28 01:20:55 -0700

    Wrote doc comments

--- android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/models/ingredient/IngredientUnit.java
+++ android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/models/ingredient/IngredientUnit.java
@@ -11,7 +11,11 @@ public class IngredientUnit {
 
     private static final IngredientUnit instance = new IngredientUnit();
 
-    private IngredientUnit(){
+    /**
+     * Private constructor creates the singleton instance of IngredientUnit and populates the
+     * default values
+     */
+    private IngredientUnit() {
         units.add("g");
         units.add("ml");
         units.add("L");
@@ -19,22 +23,48 @@ public class IngredientUnit {
         units.add("singles");
     }
 
-    public static IngredientUnit getInstance(){
+    /**
+     * The effective "Constructor". Returns the singleton instance of the class.
+     *
+     * @return The singleton instance of IngredientUnit.
+     */
+    public static IngredientUnit getInstance() {
         return instance;
     }
 
-    public ArrayList<String> getAllUnits(){
+    /**
+     * Returns an ArrayList of all currently held units.
+     *
+     * @return An {@link ArrayList} of {@link String}s with the current units.
+     */
+    public ArrayList<String> getAllUnits() {
         return units;
     }
 
-    public String getUnitFromIndex(int index){
+    /**
+     * Returns the unit stored at the given index.
+     *
+     * @param index The index of the unit to be retrieved as a primitive int.
+     * @return The unit at the input index as a {@link String}.
+     */
+    public String getUnitFromIndex(int index) {
         return units.get(index);
     }
 
-    public void addUnit(String locationToAdd){
+    /**
+     * Adds the input unit to the list of categories.
+     *
+     * @param locationToAdd The unit to be added to the list as a {@link String}.
+     */
+    public void addUnit(String locationToAdd) {
         units.add(locationToAdd);
     }
 
+    /**
+     * Deletes the given unit from the list.
+     *
+     * @param unitToDelete The unit to be removed from the list as a {@link String}.
+     */
     public void deleteUnit(String unitToDelete) {
         units.remove(unitToDelete);
     }
