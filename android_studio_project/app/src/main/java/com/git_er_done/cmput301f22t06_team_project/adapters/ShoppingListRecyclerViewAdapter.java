f9bc1cdfd53cb139104bbc4150a42df3328accb2
Joel Kennedy <jak1@ualberta.ca>
2022-11-28 04:32:09 -0700

    Ingredients purchased now  go into storage and db, Added toast message too to remind the user to edit ingredient details

--- android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/adapters/ShoppingListRecyclerViewAdapter.java
+++ android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/adapters/ShoppingListRecyclerViewAdapter.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -80,6 +81,7 @@ public class ShoppingListRecyclerViewAdapter extends RecyclerView.Adapter<Shoppi
                         selectedIngredient.getLocation(), selectedIngredient.getUnit(), selectedIngredient.getCategory(),
                         selectedIngredient.getAmount() + quantity);
                 modifyIngredientInDB(newIngredient, selectedIngredient, pos);
+                Toast.makeText(v.getContext(), "Make sure to update ingredient details!", Toast.LENGTH_LONG).show();
             }
         });
     }
