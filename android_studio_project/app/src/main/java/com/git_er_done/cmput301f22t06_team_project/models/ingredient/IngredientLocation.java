d14e7270774205df325c09ac8d6ac16306c7d006
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-11-28 01:14:17 -0700

    Wrote doc comments

--- android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/models/ingredient/IngredientLocation.java
+++ android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/models/ingredient/IngredientLocation.java
@@ -12,28 +12,58 @@ public class IngredientLocation {
 
     private static final IngredientLocation instance = new IngredientLocation();
 
+    /**
+     * Private constructor creates the singleton instance of IngredientLocation and populates the
+     * default values
+     */
     private IngredientLocation() {
         locations.add("pantry");
         locations.add("fridge");
         locations.add("freezer");
     }
 
+    /**
+     * The effective "Constructor". Returns the singleton instance of the class.
+     *
+     * @return The singleton instance of IngredientLocation.
+     */
     public static IngredientLocation getInstance() {
         return instance;
     }
 
+    /**
+     * Returns an ArrayList of all currently held locations.
+     *
+     * @return An {@link ArrayList} of {@link String}s with the current locations.
+     */
     public ArrayList<String> getAllLocations() {
         return locations;
     }
 
+    /**
+     * Returns the category stored at the given index.
+     *
+     * @param index The index of the location to be retrieved as a primitive int.
+     * @return The location at the input index as a {@link String}.
+     */
     public String getLocationFromIndex(int index) {
         return locations.get(index);
     }
 
+    /**
+     * Adds the input location to the list of categories.
+     *
+     * @param locationToAdd The location to be added to the list as a {@link String}.
+     */
     public void addLocation(String locationToAdd) {
         locations.add(locationToAdd);
     }
 
+    /**
+     * Deletes the given location from the list.
+     *
+     * @param locationToDelete The location to be removed from the list as a {@link String}.
+     */
     public void deleteLocation(String locationToDelete) {
         locations.remove(locationToDelete);
     }
