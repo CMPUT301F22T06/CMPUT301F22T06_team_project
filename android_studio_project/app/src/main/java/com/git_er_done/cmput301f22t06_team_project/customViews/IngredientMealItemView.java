2ef9d6d097de994f7b2b9c9f5fd1d28412d2dc93
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-11-27 20:06:47 -0700

    Removed unused import

--- android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/customViews/IngredientMealItemView.java
+++ android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/customViews/IngredientMealItemView.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
-import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
