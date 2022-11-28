b78db05e0f64ca833e4a61a0dea50eceec3a478a
rieberg1 <74480708+rieberg1@users.noreply.github.com>
2022-11-28 00:56:46 -0700

    Wrote Doc comments

--- android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/models/ingredient/IngredientCategory.java
+++ android_studio_project/app/src/main/java/com/git_er_done/cmput301f22t06_team_project/models/ingredient/IngredientCategory.java
@@ -12,7 +12,11 @@ public class IngredientCategory {
 
     private static final IngredientCategory instance = new IngredientCategory();
 
-    private IngredientCategory(){
+    /**
+     * Private constructor creates the singleton instance of IngredientCategory and populates the
+     * default values
+     */
+    private IngredientCategory() {
         ingredientCategories.add("dairy");
         ingredientCategories.add("fruit");
         ingredientCategories.add("grain");
@@ -23,22 +27,48 @@ public class IngredientCategory {
         ingredientCategories.add("miscellaneous");
     }
 
-    public static IngredientCategory getInstance(){
+    /**
+     * The effective "Constructor". Returns the singleton instance of the class.
+     *
+     * @return The singleton instance of IngredientCategory.
+     */
+    public static IngredientCategory getInstance() {
         return instance;
     }
 
-    public ArrayList<String> getAllIngredientCategories(){
+    /**
+     * Returns an ArrayList of all currently held categories.
+     *
+     * @return An {@link ArrayList} of {@link String}s with the current categories.
+     */
+    public ArrayList<String> getAllIngredientCategories() {
         return ingredientCategories;
     }
 
-    public String getIngredientCategoryFromIndex(int index){
+    /**
+     * Returns the category stored at the given index.
+     *
+     * @param index The index of the category to be retrieved as a primitive int.
+     * @return The category at the input index as a {@link String}.
+     */
+    public String getIngredientCategoryFromIndex(int index) {
         return ingredientCategories.get(index);
     }
 
-    public void addIngredientCategory(String locationToAdd){
-        ingredientCategories.add(locationToAdd);
+    /**
+     * Adds the input category to the list of categories.
+     *
+     * @param categoryToAdd The category to be added to the list as a {@link String}.
+     */
+    public void addIngredientCategory(String categoryToAdd) {
+        ingredientCategories.add(categoryToAdd);
     }
 
+    /**
+     * Deletes the given category from the list.
+     *
+     * @param categoryToDelete The category to be removed from the list as a {@link String}.
+     */
     public void deleteCategory(String categoryToDelete) {
         ingredientCategories.remove(categoryToDelete);
     }
